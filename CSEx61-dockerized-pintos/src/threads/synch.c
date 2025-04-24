f5c4e1923a2fc769696385e13210bcff8912607d
Alimedhat000 <alisensei000@gmail.com>
2025-04-25 01:56:16 +0300

    Remove semaphore_elem structure from synch.c to fix error

--- CSEx61-dockerized-pintos/src/threads/synch.c
+++ CSEx61-dockerized-pintos/src/threads/synch.c
@@ -393,11 +393,6 @@ lock_held_by_current_thread (const struct lock *lock)
 }
 
 /* One semaphore in a list. */
-struct semaphore_elem 
-  {
-    struct list_elem elem;              /* List element. */
-    struct semaphore semaphore;         /* This semaphore. */
-  };
 
 /* Initializes condition variable COND.  A condition variable
    allows one piece of code to signal a condition and cooperating
